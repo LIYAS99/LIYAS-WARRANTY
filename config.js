c28da8dbb0f22811c98bd14389c9b6644c44c076
LIYAS99 <powerplustechnology99@gmail.com>
2026-07-30 23:13:58 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,3 @@
+const SUPABASE_URL = "https://qhuygccueklrlmrmsavi.supabase.co/rest/v1/";
+
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFodXlnY2N1ZWtscmxtcm1zYXZpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODU0MjcyNTUsImV4cCI6MjEwMTAwMzI1NX0.168BBeR0BvrjCu-MRu2FTA05oTzXc9aXDrOWJHIejKA";
