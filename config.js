58facaf0a560d7148cba52ef36b57bb6f005c51e
LIYAS99 <powerplustechnology99@gmail.com>
2026-08-01 00:14:46 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,20 @@
+// =====================================
+// LIYAS WARRANTY PORTAL
+// config.js
+// =====================================
+
+// Apna Project URL yaha paste karo
 const SUPABASE_URL = "https://qhuygccueklrlmrmsavi.supabase.co";
 
+// Apni Anon Public Key yaha paste karo
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFodXlnY2N1ZWtscmxtcm1zYXZpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODU0MjcyNTUsImV4cCI6MjEwMTAwMzI1NX0.168BBeR0BvrjCu-MRu2FTA05oTzXc9aXDrOWJHIejKA";
 
+// Supabase Client
 const supabase = window.supabase.createClient(
     SUPABASE_URL,
     SUPABASE_ANON_KEY
 );
+
+// Test
+console.log("✅ LIYAS Config Loaded");
+console.log("Supabase Client:", supabase);
